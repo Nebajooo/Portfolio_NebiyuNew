7ff6c3c9a3e9fa8b5c9902928769afd9ed41190a
Nebajooo <nebajoo2015@gmail.com>
2024-01-15 12:12:52 -0800

    update in portfolio

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -18,7 +18,7 @@ function Home2() {
             <h1 style={{ fontSize: "2.6em" }}>
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
-            <p className="home-about-body" style={{color:'black'}}>
+            <p className="home-about-body" style={{ color: "black" }}>
               I fell in love with programming and I have at least learnt
               something, I think… 🤷‍♂️
               <br />
@@ -30,8 +30,7 @@ function Home2() {
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> 
-                
+                <b className="purple">Web Technologies and Products </b>
               </i>
               <br />
               <br />
@@ -64,41 +63,37 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/nebajooo"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
+                  className="icon-colour  home-social-icons">
                   <AiFillGithub />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com/nebiyu"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
+                  className="icon-colour  home-social-icons">
                   <AiOutlineTwitter />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/nebiyu-yohannes-52983722a"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
+                  className="icon-colour  home-social-icons">
                   <FaLinkedinIn />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/nebajooo"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
+                  className="icon-colour home-social-icons">
                   <AiFillInstagram />
                 </a>
               </li>
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -5,12 +5,7 @@ function Type() {
   return (
     <Typewriter
       options={{
-        strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          
-        ],
+        strings: ["Software Developer", "Freelancer", "MERN Stack Develope"],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
