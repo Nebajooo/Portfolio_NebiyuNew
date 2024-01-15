3a930f4b65512d7f808b50362903fc85515b80cc
Nebajooo <nebajoo2015@gmail.com>
2024-01-15 12:18:04 -0800

    update in pportfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,7 +41,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href=""
+                href="https://www.linkedin.com/in/nebiyu-yohannes-52983722a"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer">
