8321d17e561a9d901a8c78e2475ed66d341b2d4c
Nebiyu Yohannes <106951303+Nebajooo@users.noreply.github.com>
2026-02-27 09:19:41 +0300

    update the code

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -5,7 +5,7 @@ function Type() {
   return (
     <Typewriter
       options={{
-        strings: ["Software Developer", "Freelancer", "MERN Stack Develope"],
+        strings: ["Software Developer", "ERP Developer", "MERN Stack Develope"],
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
